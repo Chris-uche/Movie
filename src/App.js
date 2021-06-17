32a2f8f53af6512cf1d542ccefd3849288ba58fd
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-06-17 16:36:19 +0100

    added a functionality to the file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import React,{useState, useEffect} from "react"
 import './App.css';
+import featureEndpoint from './featureEndpoint';
+import Results from './Results';
 
 
-const App=()=>{
+const MovieApp=()=>{
+  const [movie, setMovie]= useState([]);
+  //const [searchitem, setSearchItem] = useState("")
+
+
+  useEffect(()=>{
+    fetch(featureEndpoint)
+    .then((response)=> response.json())
+    .then((response)=>{
+      setMovie(response.results)
+      //console.log(response.results)
+    })
+    
+  },[])
+  
+  
 
   return(
     <div className="App">
-      <h1>yeah</h1>
+      <Results movie={movie}/>
     </div>
   )
 
 }
 
 
-export default App;
+export default MovieApp;
