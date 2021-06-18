d9d9925b8b02cb80980710db6965b254831eeff8
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-06-18 17:21:58 +0100

    Made few code corrections on the results component

---
+++ src/Results.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Movie from './Movie.js'
+
+
+
+const Results=({movie})=>{
+    
+
+    return(
+        <>
+        <div className="container">
+            {movie === 0 ? (<h1>No Movie Found</h1>) : (
+                movie.map((movieResults)=>(
+                    <Movie key={movieResults.id}
+                    title={movieResults.title}
+                    poster_path={movieResults.poster_path}
+                    overview={movieResults.overview}
+                    vote_average={movieResults.vote_average}
+                    />
+                ))
+                
+            )}
+             
+        </div>
+
+        </>
+    )
+
+
+}
+
+export default Results
