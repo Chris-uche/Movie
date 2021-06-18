4d8f371165af8d6c3a454869c174c9cee38fe433
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-06-18 17:56:31 +0100

    added a classname on the Result component

--- src/Results.js
+++ src/Results.js
@@ -9,6 +9,7 @@ const Results=({movie})=>{
     return(
         <>
         <div className="container">
+            
             {movie === 0 ? (<h1>No Movie Found</h1>) : (
                 movie.map((movieResults)=>(
                     <Movie key={movieResults.id}
