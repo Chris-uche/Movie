e53603415e0bbef8892993a3ddee9ed9cb04fc81
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-06-18 17:21:07 +0100

    Added new functionality on the movie component

---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import imagesEndpoint from './imageEndpoint'
+
+const Movie =({title,poster_path,overview,vote_average})=>{
+    return(
+        <>
+        <div className="">
+            <img src={imagesEndpoint + poster_path} alt={title} />
+            <div className="movie-info">
+                <h3>{title}</h3>
+                <span className="">{vote_average}</span>
+
+            </div>
+            <div className="overview">
+                <h3>Overview</h3>
+                <p>{overview}</p>
+            </div>
+
+        </div>
+
+        </>
+    )
+}
+export default Movie
